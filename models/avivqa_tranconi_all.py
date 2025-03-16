958a34b74fb76f7174e709e20a87bb2eb1c04f39
XuanHiepD <hiepduong085@gmail.com>
2025-03-17 03:15:35 +0700

    New code 39

--- models/avivqa_tranconi_all.py
+++ models/avivqa_tranconi_all.py
@@ -280,6 +280,7 @@ def avivqa_model(pretrained=False, num_classes=353, **kwargs):
 
 @register_model
 def selective_avivqa_model(pretrained=False, num_classes=353, **kwargs):
+    print(kwargs)
     args = _get_base_config(**kwargs)
     model = ViVQABEiT3Selective(
         args, num_classes=num_classes, **kwargs)
