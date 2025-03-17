d61d8e531663809359974de68d0c1d0f7d838002
XuanHiepD <hiepduong085@gmail.com>
2025-03-17 13:16:58 +0700

    New code 49

--- scripts/train_vqa_mulmodels_loop.sh
+++ scripts/train_vqa_mulmodels_loop.sh
@@ -5,7 +5,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-for id in 1 2
+for id in 1 2 3 4 5 6 7 8 9 10
 do
     bash scripts/train_vqa_mulmodels.sh $id
 done
