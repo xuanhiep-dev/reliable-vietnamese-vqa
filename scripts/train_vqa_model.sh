7a90dde71176f4a81f179055651bb9ac5cc19a49
XuanHiepD <hiepduong085@gmail.com>
2025-03-24 07:05:08 +0700

    Upload new codes v9

--- scripts/train_vqa_model.sh
+++ scripts/train_vqa_model.sh
@@ -5,15 +5,19 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-use_selector=$1
-output_path=$2
+parse_args() {
+  for arg in "$@"; do
+    [[ $arg == *=* ]] && eval "${arg%%=*}='${arg#*=}'"
+  done
+}
+parse_args "$@"
+
 train_path="data/full/train.csv"
 valid_path="data/full/valid.csv"
 test_path="data/full/test.csv"
 
-
-python main.py --set model.use_selector=${use_selector} \
-                     paths.output_path=${output_path} \
-                     paths.train_path=${train_path} \
-                     paths.valid_path=${valid_path} \
-                     paths.test_path=${test_path}
+python main.py --set model.use_selector="$use_selector" \
+                     paths.output_path="$output_path" \
+                     paths.train_path="$train_path" \
+                     paths.valid_path="$valid_path" \
+                     paths.test_path="$test_path"
