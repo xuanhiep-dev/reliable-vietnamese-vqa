96cabb51605a051e094767eed484b2c7f74aff6f
XuanHiepD <hiepduong085@gmail.com>
2025-03-26 06:46:05 +0700

    Upload new codes v50

--- utils/trainer.py
+++ utils/trainer.py
@@ -67,7 +67,7 @@ class TrainingModeHandler:
         model = create_model("avivqa_model", **self._model_cfg)
         if not os.path.exists(load_path):
             print(
-                f"[INFO] load_path {load_path} not found. Creating and loading base model.")
+                f"[INFO] Load path: {load_path} not found. Creating and loading base model.")
             self.save_base_model()
 
             if not os.path.exists(self._base_model_path):
