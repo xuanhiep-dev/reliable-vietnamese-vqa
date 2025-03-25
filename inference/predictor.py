d8c769c563542712053089f2665a69cd155f60d2
XuanHiepD <hiepduong085@gmail.com>
2025-03-26 06:06:16 +0700

    Upload new codes v44

--- inference/predictor.py
+++ inference/predictor.py
@@ -7,61 +7,59 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 
-def load_config():
-    return ConfigLoader()
+class PredictorModeHandler:
+    def __init__(self):
+        self._cfg = ConfigLoader()
+        self._paths_cfg = self._cfg.get("paths")
+        self._model_cfg = self._cfg.get("model")
 
+    def load_final_model(self, model_path=None):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def load_final_model(model_path=None):
-    cfg = load_config()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        try:
+            model = create_model("avivqa_model", **self._model_cfg)
+        except Exception as e:
+            raise ValueError(f"Cannot create the model.")
 
-    try:
-        model = create_model("avivqa_model", **cfg.get("model"))
-    except Exception as e:
-        raise ValueError(f"Cannot create the model.")
+        if model_path and os.path.exists(model_path):
+            print(f"Loading weights from {model_path}")
+            state_dict = torch.load(model_path, map_location=device)
 
-    if model_path and os.path.exists(model_path):
-        print(f"Loading weights from {model_path}")
-        state_dict = torch.load(model_path, map_location=device)
+            if isinstance(state_dict, dict) and "model" in state_dict:
+                state_dict = state_dict["model"]
 
-        if isinstance(state_dict, dict) and "model" in state_dict:
-            state_dict = state_dict["model"]
+            model.load_state_dict(state_dict, strict=False)
+            print("Model loaded successfully.")
+        else:
+            print("Checkpoint path does not exist")
 
-        model.load_state_dict(state_dict, strict=False)
-        print("Model loaded successfully.")
-    else:
-        print("Checkpoint path does not exist")
+        return model.to(device).eval()
 
-    return model.to(device).eval()
+    def predict_sample(self, model, image_path, question):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(
+            f"[INFO] Inference on image: {image_path}\n Question: {question}")
 
+        sample = get_sample(self._paths_cfg, image_path, question)
+        image = sample["image"].to(device, dtype=torch.float32)
+        question_ids = sample["question"].to(device)
+        mask = sample["padding_mask"].to(device)
 
-def predict_sample(model, image_path, question):
-    cfg = load_config()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(
-        f"[INFO] Inference on image: {image_path}\n Question: {question}")
+        model.eval()
+        with torch.no_grad():
+            logits = model(image=image, question=question_ids,
+                           padding_mask=mask).logits
+            probs = torch.softmax(logits, dim=-1)
+            pred_idx = torch.argmax(probs, dim=-1).item()
+            confidence = probs[0][pred_idx].item()
 
-    sample = get_sample(cfg, image_path, question)
-    image = sample["image"].to(device, dtype=torch.float32)
-    question_ids = sample["question"].to(device)
-    mask = sample["padding_mask"].to(device)
+        answer = sample["vocab"].get(pred_idx, "I don't know")
 
-    model.eval()
-    with torch.no_grad():
-        logits = model(image=image, question=question_ids,
-                       padding_mask=mask).logits
-        probs = torch.softmax(logits, dim=-1)
-        pred_idx = torch.argmax(probs, dim=-1).item()
-        confidence = probs[0][pred_idx].item()
+        print(f"[RESULT] Answer: {answer} (Confidence: {confidence:.4f})")
+        return answer, confidence
 
-    answer = sample["vocab"].get(pred_idx, "I don't know")
-
-    print(f"[RESULT] Answer: {answer} (Confidence: {confidence:.4f})")
-    return answer, confidence
-
-
-def plot_an_image(image_path):
-    image = Image.open(image_path)
-    plt.imshow(image)
-    plt.axis('off')
-    plt.show()
+    def plot_an_image(image_path):
+        image = Image.open(image_path)
+        plt.imshow(image)
+        plt.axis('off')
+        plt.show()
