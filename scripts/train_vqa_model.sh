05de95559a1323b6bc8227700d3272c483770103
XuanHiepD <hiepduong085@gmail.com>
2025-03-26 07:45:18 +0700

    Upload new codes v62

--- scripts/train_vqa_model.sh
+++ scripts/train_vqa_model.sh
@@ -19,8 +19,8 @@ test_path="data/full/test.csv"
 
 python main.py --set training.lyp_mode="$lyp_mode" \
                      model.use_selector="$use_selector" \
-                     paths.checkpoint.save_path="$save_checkpoint_path" \
-                     paths.checkpoint.load_path="$load_checkpoint_path" \
+                     paths.checkpoints.save_path="$save_checkpoint_path" \
+                     paths.checkpoints.load_path="$load_checkpoint_path" \
                      paths.train_path="$train_path" \
                      paths.valid_path="$valid_path" \
                      paths.test_path="$test_path"
