3a7f1d5dc14403674e7dce7815a875a8017af96b
XuanHiepD <hiepduong085@gmail.com>
2025-04-03 21:06:09 +0700

    Upload new code for prediction 3

--- inference/predictor.py
+++ inference/predictor.py
@@ -3,7 +3,7 @@ import torch
 import models.model
 from utils.config import ConfigLoader
 from timm.models import create_model
-from utils.dataset import ViVQAProcessor
+from utils.dataset import Process, ViVQADataset, ViVQAProcessor
 from PIL import Image
 from torch.utils.data import DataLoader
 import pandas as pd
@@ -38,10 +38,19 @@ class PredictorModeHandler:
 
         return model.to(device).eval()
 
-    def predict_test_dataset(self, model, test_dataset, batch_size=32):
+    def get_test_dataset(self):
+        processor = Process()
+        image_path = self._paths_cfg["image_path"]
+        ans_path = self._paths_cfg["ans_path"]
+        df_test = pd.read_csv(self._paths_cfg["test_path"], index_col=0)
+        test_dataset = ViVQADataset(df_test, processor, image_path, ans_path)
+        return test_dataset
+
+    def predict_test_dataset(self, model, batch_size=32):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model.eval()
 
+        test_dataset = self.get_test_dataset()
         dataloader = DataLoader(
             test_dataset, batch_size=batch_size, shuffle=False)
 
